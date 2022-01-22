b18b6395e2770aec8a6daae514fee67b767f4a9c
BillyMiles <williammiles201@gmail.com>
2022-01-22 08:21:27 -0500

    first commit

--- src/components/ForecastShow.js
+++ src/components/ForecastShow.js
@@ -4,11 +4,7 @@ function ForecastShow() {
     const weather = useSelector(state => state.weather[0])
     // debugger
 
-    if (weather === undefined)
-        console.log("no data")
-    else
-        console.log("forecast", weather.forecast.forecastday[0]
-)
+   
   
     if (weather === undefined)
         return (
@@ -19,15 +15,17 @@ function ForecastShow() {
     else
         return (
             <div>
-                <h1>Forecast: {weather.location.name}</h1>
+                <h1>Forecast: {weather.location.name}, {weather.location.region}</h1>
                 {weather.forecast.forecastday.map((day) => (
-                    <div>
-                    <h1>Date: {day.date}</h1>
-                    <img src={day.day.condition.icon}/>
-                    <h2>High: {day.day.maxtemp_f}</h2>
-                    <h2>Low: {day.day.mintemp_f}</h2>
-                    <h3>Sunrise: {day.astro.sunrise}  Sunset: {day.astro.sunset}</h3>
-                    <br/>
+                    <div key={day.date_epoch}>
+                        <h1>Date: {day.date}</h1>
+                        <img src={day.day.condition.icon}/>
+                        <h3>{day.day.condition.text}</h3>
+                        <h3>Total Precip: {day.day.totalprecip_in} in</h3>
+                        <h2>High: {day.day.maxtemp_f}°F</h2>
+                        <h2>Low: {day.day.mintemp_f}°F</h2>
+                        <h3>Sunrise: {day.astro.sunrise}  Sunset: {day.astro.sunset}</h3>
+                        <br/>
                     </div>
                 ))}
                 
